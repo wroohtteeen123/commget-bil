41cec3461f02b00894f6eb28ce30d113487d16d3
Jincheng <38003358+jjc123-PBAS@users.noreply.github.com>
2021-11-20 11:55:26 +0800

    添加 TDoR 纪念提示！

---
+++ test/Test_4_1120.py
@@ -0,0 +1,37 @@
+from tkinter import *
+import time
+
+
+def what_day():
+
+    day_time = int(time.strftime("%m%d", time.localtime()))
+
+    if day_time == 1120:
+
+        show_window_transgender()
+
+
+def show_window_transgender():
+
+    root = Tk()
+
+    root.title("Transgender")
+
+    Button(root, text="跨性别死难者纪念日", bd = 15).pack()
+
+    Label(root, text=" " * 60, bg="light blue").pack()
+    Label(root, text=" " * 60, bg="pink").pack()
+    Label(root, text=" " * 60, bg="white").pack()
+    Label(root, text=" " * 60, bg="pink").pack()
+    Label(root, text=" " * 60, bg="light blue").pack()
+
+    Label(root, text="").pack()
+
+    Label(root, text="悼念被谋杀的跨性别者🕯").pack()
+
+    Label(root, text=" "* 70).pack()
+
+    root.mainloop()
+
+
+what_day()
